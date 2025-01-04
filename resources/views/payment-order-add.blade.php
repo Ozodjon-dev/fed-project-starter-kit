f550f5fef964208d46b957381ba908ffa0684d37
Ozodjon-dev <viperson.333@gmail.com>
2025-01-04 13:56:32 +0500

    test

--- resources/views/payment-order-add.blade.php
+++ resources/views/payment-order-add.blade.php
@@ -410,7 +410,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">×</button>
                 <div class="modal-header mb-1">
                     <h5 class="modal-title">
-                        <span class="align-middle">Сохранить</span>
+                        <span class="align-middle">Проводка</span>
                     </h5>
                 </div>
                 <div class="modal-body flex-grow-1">

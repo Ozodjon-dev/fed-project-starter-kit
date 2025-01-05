d2760e23dd00275f59d273e4c03dc725d4403a59
Ozodjon-dev <100852227+Ozodjon-dev@users.noreply.github.com>
2025-01-05 22:44:11 +0500

    test

--- resources/views/payment-order-add.blade.php
+++ resources/views/payment-order-add.blade.php
@@ -330,7 +330,17 @@
                             просмотр</a>
                         <button class="btn btn-primary w-100 mb-75" data-bs-toggle="modal"
                                 data-bs-target="#send-invoice-sidebar">
-                            Следующий >>>
+                            Следующий
+                            <svg height="13px" width="13px"
+                                 viewBox="0 0 185.343 185.343" xml:space="preserve">
+<g>
+    <g>
+        <path style="fill:#FFFFFF;" d="M51.707,185.343c-2.741,0-5.493-1.044-7.593-3.149c-4.194-4.194-4.194-10.981,0-15.175
+			l74.352-74.347L44.114,18.32c-4.194-4.194-4.194-10.987,0-15.175c4.194-4.194,10.987-4.194,15.18,0l81.934,81.934
+			c4.194,4.194,4.194,10.987,0,15.175l-81.934,81.939C57.201,184.293,54.454,185.343,51.707,185.343z"/>
+    </g>
+</g>
+</svg>
                         </button>
                     </div>
                 </div>
@@ -464,11 +474,12 @@
                         </div>
                         <div class="mb-1 flex-wrap mt-2 card-body">
                             <div class="card">
-                                    <a href="" class="btn btn-outline-primary w-100 mb-75" data-bs-dismiss="modal">Отмена</a>
-                                    <button class="btn btn-primary w-100 mb-75" data-bs-toggle="modal"
-                                            data-bs-target="#send-invoice-sidebar">
-                                        Сохранить
-                                    </button>
+                                <a href="" class="btn btn-outline-primary w-100 mb-75"
+                                   data-bs-dismiss="modal">Отмена</a>
+                                <button class="btn btn-primary w-100 mb-75" data-bs-toggle="modal"
+                                        data-bs-target="#send-invoice-sidebar">
+                                    Сохранить
+                                </button>
                             </div>
                         </div>
                     </form>

6ce59ee1883ae534d3788f373b7908adf462ae35
Ozodjon-dev <viperson.333@gmail.com>
2025-01-15 17:23:25 +0500

    activated WithHeadings to export excel contracts list

--- app/Exports/ContractExport.php
+++ app/Exports/ContractExport.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use App\Models\Contract;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class ContractExport implements FromCollection
+class ContractExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -13,5 +14,27 @@ class ContractExport implements FromCollection
     public function collection()
     {
         return Contract::all();
+    }
+
+    public function headings(): array
+    {
+        return [
+            '#',
+            'Рег. №',
+            'Дата рег.',
+            'Тип договора',
+            'Номер договора',
+            'Дата договора',
+            'Контрагент',
+            'Категория',
+            'Предмет',
+            'Статья',
+            'Сумма',
+            'Срок договора',
+            'Статус',
+            'Дата создание',
+            'Дата редактирование',
+            'Дата удаление'
+        ];
     }
 }
